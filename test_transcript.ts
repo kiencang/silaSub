1783be5c06658665968175463c798964798c0354
Nguyễn Đức Anh <78894062+kiencang@users.noreply.github.com>
2026-04-21 01:00:46 +0700

    Delete test_transcript.ts

--- test_transcript.ts
+++
@@ -1,4 +0,0 @@
-import { YoutubeTranscript } from 'youtube-transcript';
-YoutubeTranscript.fetchTranscript('M576WlNmIoo')
-    .then(t => console.log('M576WlNmIoo DEFAULT: SUCCESS length', t.length))
-    .catch(e => console.error('M576WlNmIoo DEFAULT ERROR: ', e.message));
