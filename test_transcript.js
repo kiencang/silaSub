4a26df7b4a45faa352520289e0fd2931760b5ea5
Nguyễn Đức Anh <78894062+kiencang@users.noreply.github.com>
2026-04-21 01:00:49 +0700

    Delete test_transcript.js

--- test_transcript.js
+++
@@ -1,8 +0,0 @@
-const { YoutubeTranscript } = require('youtube-transcript');
-YoutubeTranscript.fetchTranscript('5JoKCduKKyk')
-    .then(t => console.log('DEFAULT: ', t.slice(0,2)))
-    .catch(e => console.error('DEFAULT ERROR: ', e.message));
-
-YoutubeTranscript.fetchTranscript('5JoKCduKKyk', { lang: 'en' })
-    .then(t => console.log('EN: ', t.slice(0,2)))
-    .catch(e => console.error('EN ERROR: ', e.message));
